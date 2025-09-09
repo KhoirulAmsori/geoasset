3838288fe0abc22c2d7f845bbd7eb515e47c211f
Khoirul Amsori <khoirul.amsori@gmail.com>
2025-09-09 21:26:52 +0700

    Update proxy name

--- proxy-build/Collector/ProxyCollector.cs
+++ proxy-build/Collector/ProxyCollector.cs
@@ -123,7 +123,7 @@ public class ProxyCollector
                                         .Trim();
 
                         // Daftar suffix formal yang ingin dihapus
-                        var formalSuffixes = new[] { "SAS", "INC", "LTD", "LLC", "CORP", "CO", "SA", "SRO", "ASN", "LIMITED", "COMPANY", "ASIA", "CLOUD" };
+                        var formalSuffixes = new[] { "SAS", "INC", "LTD", "LLC", "CORP", "CO", "SA", "SRO", "ASN", "LIMITED", "COMPANY", "ASIA", "CLOUD", "INTERNATIONAL", "BRAINSTORM", "PROVIDER", "ISLAND" };
 
                         // Pisahkan kata
                         var ispParts = ispRaw.Split(new[] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries)
