7540f69941f0d9e71961cb1fa93883af26d710a7
Khoirul Amsori <khoirul.amsori@gmail.com>
2025-09-11 08:04:21 +0700

    Fix json import

--- proxy-build/Collector/ProxyCollector.cs
+++ proxy-build/Collector/ProxyCollector.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Text;
-// using System.Text.Json;
+using System.Text.Json;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Threading.Tasks;
